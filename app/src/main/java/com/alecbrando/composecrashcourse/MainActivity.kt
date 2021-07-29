25c1f06e1068845e660fc620deeb23ec3cd08044
alecbrando <37994059+alecbrando@users.noreply.github.com>
2021-07-29 16:53:24 -0400

    using constraints in compose

--- app/src/main/java/com/alecbrando/composecrashcourse/MainActivity.kt
+++ app/src/main/java/com/alecbrando/composecrashcourse/MainActivity.kt
@@ -3,45 +3,46 @@ package com.alecbrando.composecrashcourse
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.gestures.Orientation
-import androidx.compose.foundation.gestures.scrollable
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.*
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import kotlinx.coroutines.launch
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.ConstraintSet
+import androidx.constraintlayout.compose.Dimension
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val constraint = ConstraintSet {
+                val greenBox = createRefFor("greenbox")
+                val redBox = createRefFor("redbox")
 
-            LazyColumn {
-                itemsIndexed(
-                    listOf("This", "is", "Jet", "Pack", "Compose", "This", "is", "Jet", "Pack", "Compose", "This", "is", "Jet", "Pack", "Compose")
-                ){  index, currentString ->
-                    Text(
-                        text = currentString,
-                        fontSize = 24.sp,
-                        fontWeight = FontWeight.Bold,
-                        textAlign = TextAlign.Center,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(vertical = 24.dp)
-                    )
+                constrain(greenBox) {
+                    top.linkTo(parent.top)
+                    start.linkTo(parent.start)
+                    width = Dimension.value(100.dp)
+                    height = Dimension.value(100.dp)
                 }
-
+                constrain(redBox) {
+                    top.linkTo(parent.top)
+                    start.linkTo(greenBox.end)
+                    end.linkTo(parent.end)
+                    width = Dimension.value(100.dp)
+                    height = Dimension.value(100.dp)
+                }
+                createHorizontalChain(greenBox, redBox)
+            }
+            ConstraintLayout(constraint, modifier = Modifier.fillMaxSize()) {
+                Box(modifier = Modifier.background(Color.Green)
+                    .layoutId("greenbox"))
+                Box(modifier = Modifier.background(Color.Red)
+                    .layoutId("redbox"))
             }
         }
     }
