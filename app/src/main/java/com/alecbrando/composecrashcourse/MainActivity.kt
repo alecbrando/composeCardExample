2c8ea58cc5d84d731a33de79f4832641769c7ef1
alecbrando <37994059+alecbrando@users.noreply.github.com>
2021-07-29 16:29:41 -0400

    scrollable list itemsIndex

--- app/src/main/java/com/alecbrando/composecrashcourse/MainActivity.kt
+++ app/src/main/java/com/alecbrando/composecrashcourse/MainActivity.kt
@@ -3,13 +3,22 @@ package com.alecbrando.composecrashcourse
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import kotlinx.coroutines.launch
 
 
@@ -17,38 +26,22 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val scaffoldState = rememberScaffoldState()
-            var textFieldState by remember {
-                mutableStateOf("")
-            }
 
-            val scope = rememberCoroutineScope()
-
-            Scaffold(modifier = Modifier.fillMaxSize(), scaffoldState = scaffoldState) {
-                Column(
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.Center,
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(horizontal = 30.dp)
-                ) {
-                    TextField(
-                        value = textFieldState,
-                        onValueChange = {
-                            textFieldState = it
-                        }, label = { Text("Enter your name") },
-                        singleLine = true,
-                        modifier = Modifier.fillMaxWidth()
+            LazyColumn {
+                itemsIndexed(
+                    listOf("This", "is", "Jet", "Pack", "Compose", "This", "is", "Jet", "Pack", "Compose", "This", "is", "Jet", "Pack", "Compose")
+                ){  index, currentString ->
+                    Text(
+                        text = currentString,
+                        fontSize = 24.sp,
+                        fontWeight = FontWeight.Bold,
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(vertical = 24.dp)
                     )
-                    Spacer(modifier = Modifier.height(16.dp))
-                    Button(onClick = {
-                        scope.launch {
-                            scaffoldState.snackbarHostState.showSnackbar(message = "Hello $textFieldState")
-                        }
-                    }) {
-                        Text(text = "Please greet me")
-                    }
                 }
+
             }
         }
     }
